f0e9fb4a7131342aec4d59960356204953d6cf40
VMeiners <64986104+VMeiners@users.noreply.github.com>
2023-05-06 13:43:11 -0600

    Add files via upload

---
+++ BasicNameAgeCalculator.py
@@ -0,0 +1,6 @@
+# Valerie Meiners
+user_name = input('What is your name?')
+user_age = int(input('How old are you?'))
+year = int(2023) - user_age
+
+print(f'Hello {user_name}! You were born in {year}.')
